db03207dd6259bae1e73e9585b626aef7de61005
Unknown <218435@student.pwr.edu.pl>
2018-06-25 15:13:09 +0200

    Konfiguracja 4
    - konfiguracja beanów za pomocą klasy konfiguracyjnej @Configuration

--- kursspring/src/main/java/com/example/kursspring/config/MainConfig.java
+++ kursspring/src/main/java/com/example/kursspring/config/MainConfig.java
@@ -1,13 +1,15 @@
 package com.example.kursspring.config;
 
+import com.example.kursspring.domain.Castle;
 import com.example.kursspring.domain.Knight;
 import com.example.kursspring.domain.Quest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
-@ImportResource("classpath:config/castle-config.xml")
+@PropertySource("classpath:castle.properties")
 public class MainConfig {
 
     @Bean
@@ -20,5 +22,13 @@ public class MainConfig {
         Knight knight = new Knight("Lancelot", 29);
         knight.setQuest(createQuest());
         return knight;
+    }
+
+    @Bean(name = "zamek",initMethod = "build", destroyMethod = "tearDown")
+    @Value("${my.castle.name:East Watch}")
+    public Castle castle(String name){
+        Castle castle = new Castle(knight());
+        castle.setName(name);
+        return castle;
     }
 }
